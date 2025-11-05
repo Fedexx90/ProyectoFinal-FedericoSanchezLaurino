96fce69038ca396b171edacce758d35a719d2d8f
Federico Nicolas Sanchez Laurino <fsanchezlaurino@frba.utn.edu.ar>
2025-11-05 18:35:34 -0300

    Corrijo urls.py y configuro acceso al admin

--- playground/urls.py
+++ playground/urls.py
@@ -6,16 +6,17 @@ from . import views
 from .views import AboutView
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+
+    # 🔹 Rutas principales de la web
+    path('', views.home, name='home'),
     path('cars/', include('cars.urls')),
     path('accounts/', include('accounts.urls')),
     path('about/', views.about, name='about'),
-    path('messages/', include('messages_app.urls')),  
+    path('messages/', include('messages_app.urls')),
 ]
 
+# 🔹 Archivos estáticos y de medios
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
